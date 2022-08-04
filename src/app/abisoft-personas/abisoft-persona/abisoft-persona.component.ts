6dfe239bd2804fbe63985b9d2932df45982b4ab6
hvisrael <hvisrael@hotmail.com>
2022-08-04 02:27:36 -0500

    111

--- src/app/abisoft-personas/abisoft-persona/abisoft-persona.component.ts
+++ src/app/abisoft-personas/abisoft-persona/abisoft-persona.component.ts
@@ -57,9 +57,29 @@ updateRecord(form: NgForm)
 
 }
 
-
-   resetForm(form:NgForm){
+resetForm(form:NgForm){
     form.form.reset();
     this.service.formData = new AbisoftPersona();
    }
+
+
+   public CalculateAge(){
+      if(this.service.formData.dob){
+        const bdate = new Date(this.service.formData.dob);
+        const timeDiff = Math.abs(Date.now() - bdate.getTime() );
+        this.service.formData.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
+      }
+      return this.service.formData.age;
+    }
+
+    public CalculateCosto(){
+      if(this.service.formData.inscriptionDate){
+        const idate = new Date(this.service.formData.inscriptionDate);
+        const timeDiff = Math.abs(Date.now()-idate.getTime());
+        const year =  Math.floor((timeDiff / (1000 * 3600 * 24)) / 365);
+         this.service.formData.cost = String(year*200);
+      }
+      return this.service.formData.cost;
+    }
+
 }
